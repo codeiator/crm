037dd091f2d60e86f918f4cd2c95721bd47d03fc
Saurabh <saurabh77164@gmail.com>
2021-02-22 23:30:13 +0530

    various changes

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 //$conn = new mysqli("localhost","awaasonl_awaas","Razorback89","awaasonl_awaas");
 //$con=mysqli_connect("localhost", "root", "", "crm");
-$con=mysqli_connect("localhost", "root", "", "awaas_crm");
+$con=mysqli_connect("localhost", "root", "", "crm");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
@@ -11,7 +11,7 @@ echo "Connection Fail".mysqli_connect_error();
 
 //$conn = new mysqli("localhost","awaasonl_awaas","Razorback89","awaasonl_awaas");
 //$con=mysqli_connect("localhost", "root", "", "crm");
-$con2=mysqli_connect("localhost", "root", "", "awaasonl_awwas");
+$con2=mysqli_connect("localhost", "root", "", "awaas");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
