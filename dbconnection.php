52e6def65b356b6bf8739351b9a43036707e154b
Saurabh <saurabh77164@gmail.com>
2021-03-15 23:15:18 +0530

    test

--- dbconnection.php
+++ dbconnection.php
@@ -14,9 +14,9 @@ echo "Connection Fail".mysqli_connect_error();
 //$con2 = new mysqli("localhost","root","Awaas@12389","awaasonl_awaas");
 
 //lalit local
-$con2 = new mysqli("localhost","root","","awaasonl_awwas");
+//$con2 = new mysqli("localhost","root","","awaasonl_awwas");
 
-//$con2=mysqli_connect("localhost", "root", "", "awaas");
+$con2=mysqli_connect("localhost", "root", "", "awaas");
 
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
